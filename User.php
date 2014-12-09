78d34e933c738f7f843c53cbd83fe6e565b4682b
David Crush <dlcrush@mail.roanoke.edu>
2014-12-08 20:49:24 -0500

    further documentation

--- User.php
+++ User.php
@@ -40,40 +40,27 @@
 			$this->birth_date = $birth_date;
 		}
 
-		// Returns the json representation of the user
-		public function toJson() {
-			$data = array('username' => $this->username, 'password' => $this->password,
-				'first_name' => $this->first_name, 'last_name' => $this->last_name,
-				'birth_date' => $this->birth_date, 'sign_up_date' => $this->sign_up_date);
-			return json_encode($data);
-		}
-
-		public function toArray() {
-			$data = array('username' => $this->username, 'password' => $this->password,
-				'first_name' => $this->first_name, 'last_name' => $this->last_name,
-				'birth_date' => $this->birth_date, 'sign_up_date' => $this->sign_up_date);
-
-			return $data;
-		}
-		
-
 		// Gets the username of the user
 		public function getUsername() {
 			return $this->username;
 		}
 
+		// Gets the first_name of the user
 		public function getFirstName() {
 			return $this->first_name;
 		}
 
+		// Gets the last_name of the user
 		public function getLastName() {
 			return $this->last_name;
 		}
 
+		// Gets the birth_date of the user
 		public function getBirthDate() {
 			return $this->birth_date;
 		}
 
+		// gets the sign_up_date of the user
 		public function getSignUpDate() {
 			return $this->sign_up_date;
 		}
@@ -82,5 +69,20 @@
 		// Returns true if password matches the one on file, false otherwise.
 		public function authenticate($password) {
 			return $password == $this->password;
+		}
+
+		// Returns the json representation of the user
+		public function toJson() {
+			$data = $this->toArray();
+			return json_encode($data);
+		}
+
+		// Returns an array representation of the user
+		public function toArray() {
+			$data = array('username' => $this->username, 'password' => $this->password,
+				'first_name' => $this->first_name, 'last_name' => $this->last_name,
+				'birth_date' => $this->birth_date, 'sign_up_date' => $this->sign_up_date);
+
+			return $data;
 		}
 	}
--- UsersCollection.php
+++ UsersCollection.php
@@ -4,57 +4,67 @@
 	require_once 'UsernameAlreadyExistsException.php';
 	require_once 'UserNotFoundException.php';
 
+	// A collection of User objects
 	class UsersCollection {
 		private $users; // holds list of all user objects
 
+		// constructor
 		public function __construct () {
 			$this->users = array();
 		}
 
-		public function toArray() {
-			return $this->users;
-		}
-
+		// Reads in a JSON file representation into a collection
 		public function read($filename='data/users.json') {
 			$data = json_decode(file_get_contents($filename), true);
+
+			if ($data == null) {
+				$data = array();
+			}
 
 			foreach($data as $key => $value) {
 				$this->users[$key] = new User($value['username'], $value['password'], $value['first_name'], $value['last_name'], $value['birth_date'], $value['sign_up_date']);
 			}
-
-			//$this->users = json_decode(file_get_contents($filename), true);
-
-			// if ($this->users == null) {
-			// 	$this->users = array();
-			// }
 		}
 
+		// Writes the collection of User objects to a JSON file
 		public function save($filename='data/users.json') {
-			$data = array();
-			foreach($this->users as $user) {
-				//var_dump($user);
-				$data[$user->getUsername()] = $user->toArray();
-			}
+			$data = $this->toJson();
 
 			file_put_contents($filename, json_encode($data));
 		}
 
+		// Adds a new User object to the collection
 		public function add($user) {
 			if (array_key_exists($user->getUsername(), $this->users)) {
 				throw new UsernameAlreadyExistsException('Username already exists');
 			}
 
-			$this->users[$username] = $user;
-
-			//array_push($users, $user);
+			$this->users[$user->getUsername()] = $user;
 		}
 
+
+		// Gets the particular user, throws UserNotFoundException if username is not found
 		public function getUser($username) {
 			if (! array_key_exists($username, $this->users)) {
 				throw new UserNotFoundException('User not found.');
 			}
 
 			return $this->users[$username];
+		}
+
+		// Returns the array represnetation of the collection
+		public function toArray() {
+			return $this->users;
+		}
+
+		// Returns the JSON representation of the collection
+		public function toJson() {
+			$data = array();
+			foreach($this->users as $user) {
+				$data[$user->getUsername()] = $user->toArray();
+			}
+
+			return $data;
 		}
 	}
 
